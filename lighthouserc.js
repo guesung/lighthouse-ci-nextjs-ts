c8138e1d02164d43ed5496f1a0d0bc0ef50a8e22
guesung <62178788+guesung@users.noreply.github.com>
2024-04-16 16:29:52 +0900

    feat: edit lighthouserc

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,6 +1,13 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ["http://localhost:3000"],
+      collect: {
+        numberOfRuns: 5,
+      },
+    },
     upload: {
+      startServerCommand: "npm run start",
       target: "temporary-public-storage",
     },
   },
