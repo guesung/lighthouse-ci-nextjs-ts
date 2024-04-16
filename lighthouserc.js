2be6e8a57843241b83735d21e442af01271b688f
guesung <62178788+guesung@users.noreply.github.com>
2024-04-16 15:23:43 +0900

    feat: edit lighthouserc

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,18 @@
 module.exports = {
   ci: {
+    collect: {
+      startServerCommand: "yarn dev",
+      url: ["http://localhost:3000"],
+      collect: {
+        numberOfRuns: 3,
+      },
+      settings: {
+        preset: "desktop",
+      },
+    },
+    assert: {
+      preset: "lighthouse:recommended",
+    },
     upload: {
       target: "temporary-public-storage",
     },
