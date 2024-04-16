6eff3ccc7c828ac732532f61bcc93a27785fc0ad
guesung <62178788+guesung@users.noreply.github.com>
2024-04-16 16:24:19 +0900

    fix: remove useless lighthouse code

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,18 +1,5 @@
 module.exports = {
   ci: {
-    collect: {
-      startServerCommand: "npm run start",
-      url: ["http://localhost:3000"],
-      collect: {
-        numberOfRuns: 3,
-      },
-      settings: {
-        preset: "desktop",
-      },
-    },
-    assert: {
-      preset: "lighthouse:recommended",
-    },
     upload: {
       target: "temporary-public-storage",
     },
